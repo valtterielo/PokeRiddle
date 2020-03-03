9e131bcab5bed23865ee790d535401aa1cd8c8ae
Valtteri Elo <bgh291@myy.haaga-helia.fi>
2020-03-03 14:05:34 +0200

    added functionalities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
 
@@ -8,12 +8,20 @@ function App() {
   const [dexEntry, setDexEntry] = React.useState("")
   const [img, setImg] = React.useState("./PixelArt.png")
   const [score, setScore] = React.useState(0)
+  const [btnText, setBtnText] = React.useState("Start!")
+  const [usedNumbers, setUsedNumbers] = React.useState([])
 
   const getPokemon = () => {
 
     //gen 1 pokedex has a total of 151 entries, this chooses one pokemon randomly
-    const dexNro = Math.floor(Math.random() * 151 + 1);
+    let dexNro = Math.floor(Math.random() * 151 + 1)
 
+    //making sure that every pokemon is shown only once
+    while (usedNumbers.includes(dexNro)){
+      dexNro = Math.floor(Math.random() * 151 + 1)
+    }
+    setUsedNumbers(usedNumbers.concat(dexNro))
+    console.log(usedNumbers)
 
     //fetching the randomly chosen pokemon's name and picture
     fetch("https://pokeapi.co/api/v2/pokemon-species/" + dexNro)
@@ -30,7 +38,7 @@ function App() {
 
   //checking if the user given answer matches pokemon's correct name
   const checkAnswer = (event) => {
-    if (event.target.value == name) {
+    if (event.target.value === name) {
       document.getElementById("pokename").value = ""
       getPokemon()
       setScore(score + 1)
@@ -40,9 +48,9 @@ function App() {
   return (
     <div className="App">
       <h1>PokéRiddle</h1>
-      <p><img src={img}></img></p>
+      <p><img src={img} className="picture"></img></p>
       <input onChange={checkAnswer} type="text" id="pokename" name="pokename"></input>
-      <button onClick={getPokemon}>New Pokemon</button>
+      <button onClick={() => {getPokemon(); setBtnText("I don't know")}}>{btnText}</button>
       <p>Score: {score}/151</p>
     </div>
   );
